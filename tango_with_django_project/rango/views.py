69bd7da22a1ab757f34e19505569d2e619e8bdd6
Sabina Alexandru <2577438a@student.gla.ac.uk>
2022-02-10 20:02:58 +0000

    ch6 e

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,12 +5,15 @@ from rango.models import Page
 from django.http import HttpResponse
 def index(request):
 
-	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {}
-	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-	context_dict['categories'] = category_list
-
-	return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    
+    
+    return render(request, 'rango/index.html', context=context_dict)
 def about(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage matches to {{ boldmessage }} in the template!
@@ -23,7 +26,6 @@ def about(request):
 
 	return render(request, 'rango/about.html', context=context_dict)
 def show_category(request, category_name_slug):
-
     context_dict = {}
     try:
         
@@ -38,4 +40,5 @@ def show_category(request, category_name_slug):
   
         context_dict['category'] = None
         context_dict['pages'] = None
+        # Go render the response and return it to the client. 
     return render(request, 'rango/category.html', context=context_dict)
